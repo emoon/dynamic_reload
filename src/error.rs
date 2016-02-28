14f04c61caa35ab99fe6105ed22072078f955dd4
Daniel Collin <daniel@collin.com>
2016-02-28 10:18:36 +0100

    Added docs + Rustfmt pass

--- src/error.rs
+++ src/error.rs
@@ -3,41 +3,64 @@ use std::io;
 use std::fmt;
 use std::path::PathBuf;
 
+
+/// Errors that can be return from various operatiors
+///
 #[derive(Debug)]
 pub enum Error {
-  Load(io::Error),
-  Copy(io::Error, PathBuf, PathBuf),
-  CopyTimeOut(PathBuf, PathBuf),
-  Find(String)
+    /// Loading a library failed
+    Load(io::Error),
+    /// File copy operation failed
+    Copy(io::Error, PathBuf, PathBuf),
+    /// Timeout of file copy happend.
+    CopyTimeOut(PathBuf, PathBuf),
+    /// Failed to find library
+    Find(String),
 }
 
 impl StdError for Error {
-  fn description(&self) -> &str {
-    match *self {
-      Error::Load(_) => "Unable to load library",
-      Error::Copy(_, _, _) => "Unable to copy",
-      Error::CopyTimeOut(_, _) => "Unable to copy due to time out",
-      Error::Find(_) => "Unable to find",
+    fn description(&self) -> &str {
+        match *self {
+            Error::Load(_) => "Unable to load library",
+            Error::Copy(_, _, _) => "Unable to copy",
+            Error::CopyTimeOut(_, _) => "Unable to copy due to time out",
+            Error::Find(_) => "Unable to find",
+        }
     }
-  }
 
-  fn cause(&self) -> Option<&StdError> {
-    match *self {
-      Error::Load(ref e) => e.cause(),
-      Error::Copy(ref e, _, _) => e.cause(),
-      Error::CopyTimeOut(_, _) => None,
-      Error::Find(_) => None,
+    fn cause(&self) -> Option<&StdError> {
+        match *self {
+            Error::Load(ref e) => e.cause(),
+            Error::Copy(ref e, _, _) => e.cause(),
+            Error::CopyTimeOut(_, _) => None,
+            Error::Find(_) => None,
+        }
     }
-  }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-      match *self {
-        Error::Load(ref e) => write!(fmt, "{} {}\nDue to: {:?}", self.description(), e.description(), self.cause()),
-        Error::Copy(ref e, ref src, ref dest) => write!(fmt, "{} {:?} to {:?}\n{}\nDue to: {:?}", self.description(), src, dest, e.description(), self.cause()),
-        Error::CopyTimeOut(ref src, ref dest) => write!(fmt, "{} {:?} to {:?}", self.description(), src, dest),
-        Error::Find(ref name) => write!(fmt, "{} {}", self.description(), name),
-      }
+        match *self {
+            Error::Load(ref e) => {
+                write!(fmt,
+                       "{} {}\nDue to: {:?}",
+                       self.description(),
+                       e.description(),
+                       self.cause())
+            }
+            Error::Copy(ref e, ref src, ref dest) => {
+                write!(fmt,
+                       "{} {:?} to {:?}\n{}\nDue to: {:?}",
+                       self.description(),
+                       src,
+                       dest,
+                       e.description(),
+                       self.cause())
+            }
+            Error::CopyTimeOut(ref src, ref dest) => {
+                write!(fmt, "{} {:?} to {:?}", self.description(), src, dest)
+            }
+            Error::Find(ref name) => write!(fmt, "{} {}", self.description(), name),
+        }
     }
 }
